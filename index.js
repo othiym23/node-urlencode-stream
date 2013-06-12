827216c84601e7257de487813c5756787d91ecc7
Forrest L Norvell <forrest@newrelic.com>
2013-06-12 14:37:43 -0700

    Better error handling and optional %20 -> +.

--- index.js
+++ index.js
@@ -5,19 +5,38 @@ var Transform = require('stream').Transform;
 
 if (!Transform) Transform = require('readable-stream').Transform;
 
+function rfc3986Encode(str, spaceToPlus) {
+  var encoded = encodeURIComponent(str)
+    .replace(/!/g,   '%21')
+    .replace(/'/g,   '%27')
+    .replace(/\(/g,  '%28')
+    .replace(/\)/g,  '%29')
+    .replace(/\*/g,  '%2A');
+
+  if (spaceToPlus) return encoded.replace(/%20/g, '+');
+  else return encoded;
+}
+
 function URLEncodeStream(options) {
   if (!(this instanceof URLEncodeStream)) return new URLEncodeStream(options);
 
   Transform.call(this, options);
+
+  if (options) this.spaceToPlus = options.spaceToPlus;
 }
 util.inherits(URLEncodeStream, Transform);
 
 URLEncodeStream.prototype._transform = function (chunk, encoding, callback) {
   if (encoding === 'buffer') encoding = 'binary';
 
-  this.push(encodeURIComponent(chunk.toString(encoding)));
+  try {
+    this.push(rfc3986Encode(chunk.toString(encoding), this.spaceToPlus));
 
-  return callback();
+    return callback();
+  }
+  catch (e) {
+    return callback(e);
+  }
 };
 
 module.exports = URLEncodeStream;
--- test/urlencode-stream.tap.js
+++ test/urlencode-stream.tap.js
@@ -33,3 +33,33 @@ test("should handle being passed buffers", function (t) {
   }, "shouldn't barf on a buffer");
   urler.end();
 });
+
+test("should error when given a malformed chunk", function (t) {
+  t.plan(1);
+
+  var urler = new URLEncodeStream();
+  urler.on('error', function (error) {
+    t.ok(error, "Error should be found.");
+  });
+
+  urler.pipe(concat(function () {
+    t.fail("should never reach here");
+  }));
+
+  urler.write(String.fromCharCode(0xDFFF) + String.fromCharCode(0xE000));
+});
+
+test("should replace spaces with pluses when asked", function (t) {
+  t.plan(2);
+
+  var urler = new URLEncodeStream({spaceToPlus : true});
+  urler.pipe(concat(function (data) {
+    t.equal(data.toString('ascii'), "%5B1%2C+2%2C+3%2C+4%2C+5%5D",
+            "should encode buffer correctly");
+  }));
+
+  t.doesNotThrow(function () {
+    urler.write(new Buffer('[1, 2, 3, 4, 5]'));
+  }, "shouldn't barf on a buffer");
+  urler.end();
+});
